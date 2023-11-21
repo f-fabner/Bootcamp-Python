1bd77806266ec1ec68115a9e4df3eaec1cabba92
Fernando Fábner <fernandofabner@gmail.com>
2023-11-21 17:21:48 -0300

    Create Name_Band_Generator.py

---
+++ Days/Day_001/Name_Band_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+a = input(str("What's name of the city you grew up in?\n"))
+
+b = input(str("What's your pet's name?\n"))
+
+print(f"Your band name could be {a} {b}")
