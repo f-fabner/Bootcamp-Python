008acec14a2dcc66a5d98103b02c7ad658db017d
Fernando Fábner <fernandofabner@gmail.com>
2024-01-30 14:49:36 -0300

    day 015-1

---
+++ Days/Day_015/main.py
@@ -0,0 +1,37 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+#To Do: 1. Print Report of all coffe machine resourses
+#To Do: 2. Check resources sufficient
+#To Do: 3. Process coins
+#To Do: 4. Check transaction successful
+#To Do: 55. Make Coffee
