581d9ec86a5019924fd83314d2e8a42abb573d0b
Fernando Fábner <fernandofabner@gmail.com>
2024-03-13 14:31:15 -0300

    day 29 - 02

--- Days/Day_029/main.py
+++ Days/Day_029/main.py
@@ -1,16 +1,52 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
 LILAC = "#d68dfc"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 def generate():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    generated_password.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def buttonadd():
-    pass
+    website = input_site.get()
+    username = input_id.get()
+    password = generated_password.get()
+    data = f"{website} | {username} | {password}"
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Error!", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"These are the details entered: \nWebsite: {website}\nEmail: {username}\nPassword: {password}\nIs it ok to save?")
+
+        if is_ok:
+            with open("N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_029/data.txt", "a") as file:
+                file.write(data + "\n")
+                input_site.delete(0, END)
+                generated_password.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -25,6 +61,7 @@ padlock_img = PhotoImage(
 canvas.create_image(100, 100, image=padlock_img)
 canvas.grid(column=1, row=0, padx=(0, 100))
 
+# ********LABELS********
 # Label website
 web_label = Label(text="Website:", bg=LILAC)
 web_label.grid(column=0, row=1, padx=(10, 0))
@@ -37,18 +74,22 @@ id_label.grid(column=0, row=2, padx=(10, 0))
 pass_label = Label(text="Password:", bg=LILAC)
 pass_label.grid(column=0, row=3, padx=(10, 0))
 
+# ********ENTRYS********
 # Entry website
 input_site = Entry(width=45)
 input_site.grid(column=1, row=1, columnspan=2, padx=(0, 100))
+input_site.focus()
 
 # Entry email/username
 input_id = Entry(width=45)
 input_id.grid(column=1, row=2, columnspan=2, padx=(0, 100))
+input_id.insert(0, "fernandofabner@gmail.com")
 
 # Entry blank
 generated_password = Entry(width=20)
 generated_password.grid(column=1, row=3, padx=(0, 250))
 
+# ********BUTTONS********
 # Button Generate
 button_generate_password = Button(
     width=18, text="Generate Password", command=generate)
