592d4a2c2914ad8c85d5896404c4d6644742e410
Fernando Fábner <fernandofabner@gmail.com>
2024-03-14 15:27:04 -0300

    day 31 - 02

--- Days/Day_031/main.py
+++ Days/Day_031/main.py
@@ -1,2 +1,78 @@
+from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv(
+    "N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_031/data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+# ************************DEFS************************
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    current_card["French"]
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_card_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_card_img)
+
+
+def right():
+    pass
+
+
+def wrong():
+    pass
+
+# ************************UI************************
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+
+canvas = Canvas(height=526, width=800)
+front_card_img = PhotoImage(
+    file="N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_031/images/card_front.png")
+card_background = canvas.create_image(400, 263, image=front_card_img)
+card_title = canvas.create_text(
+    400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+
+back_card_img = PhotoImage(
+    file="N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_031/images/card_back.png")
+right_img = PhotoImage(
+    file="N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_031/images/right.png")
+wrong_img = PhotoImage(
+    file="N:/Programando/ProjetosGit/Bootcamp-Python/Days/Day_031/images/wrong.png")
+
+
+# ************************BUTTONS************************
+# Button ADD
+button_right = Button(image=right_img, highlightthickness=0, command=next_card)
+button_right.grid(column=1, row=1)
+# Button Search
+button_wrong = Button(image=wrong_img, highlightthickness=0, command=next_card)
+button_wrong.grid(column=0, row=1)
+
+next_card()
+
+window.mainloop()
