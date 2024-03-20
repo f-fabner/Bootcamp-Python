a11981daa4831a38e62a47486ae27bc8bfde70e7
Fernando Fábner <fernandofabner@gmail.com>
2024-03-20 15:52:51 -0300

    day 33 - 02

--- Days/Day_033/Training/main.py
+++ Days/Day_033/Training/main.py
@@ -1,4 +1,10 @@
 import requests
+from datetime import datetime
+
+MY_LAT = -22.740023
+MY_LONG = -47.330877
+
+'''
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -10,3 +16,23 @@ latitude = data["iss_position"]["latitude"]
 iss_position = (longitude, latitude)
 
 print(iss_position)
+'''
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+reponse = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+reponse.raise_for_status()
+data = reponse.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print (time_now.hour)
